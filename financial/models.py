6a43bb2f5105316eb54b5b7435128457042d786f
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-18 11:20:14 +0330

    Modified financial model

--- financial/models.py
+++ financial/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# financial/models.py
+from django.db import models
+from cart.models import ShoppingCart
+from django.contrib.auth.models import User
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shopping_cart = models.OneToOneField(ShoppingCart, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=20, choices=[('success', 'Success'), ('failure', 'Failure')])
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} - {self.payment_status}"
