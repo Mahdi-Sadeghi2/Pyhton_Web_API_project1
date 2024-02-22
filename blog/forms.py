e9e3565ea3fc0e7bceb9d2f57d50578ad224912c
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-22 14:38:14 +0330

    Modified form in blog app

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+# forms.py
+from django import forms
+from .models import UserComment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = UserComment
+        fields = ['comment_text']
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=150, required=True)
+    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+    password = forms.CharField(max_length=150)
+    
+    class Meta:
+        model= User
+        fields = ('username', 'email', 'password')
