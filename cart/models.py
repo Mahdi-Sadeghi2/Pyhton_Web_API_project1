743a078e062d2c3eaa5536c86a44c74823c5eed6
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-22 14:40:39 +0330

    Added models in cart app

--- cart/models.py
+++ cart/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+
+# Create your models here.
+
 
 class ShoppingCart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='CartItem')
     is_paid = models.BooleanField(default=False)
-
+    
+    class Meta:
+        verbose_name = 'shopping Cart'
+        verbose_name_plural = 'shopping Carts'
+        ordering = ('id',)
+    
     def __str__(self):
         return f"Shopping Cart for {self.user.username}"
 
@@ -18,5 +23,10 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
+    class Meta:
+        verbose_name = 'Cart Item'
+        verbose_name_plural = 'Cart Items'
+        ordering = ('id',)
+    
     def __str__(self):
         return f"{self.quantity} x {self.product.name} in {self.shopping_cart}"
