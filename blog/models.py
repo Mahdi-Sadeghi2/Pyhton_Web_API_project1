f8f13ee1bd84e825e8428d68b02219a2af69684b
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-22 14:38:49 +0330

    Added some modification in models of blog app

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-# Create your models here.
-# wings/models.py
-from django.db import models
 from django.contrib.auth.models import User
 
 class ContentCategory(models.Model):
@@ -17,8 +13,13 @@ class Content(models.Model):
     description = models.TextField()
     image_gallery = models.ManyToManyField('Image', related_name='content_images')
     video_gallery = models.ManyToManyField('Video', related_name='content_videos')
-    user_comments = models.ManyToManyField('UserComment', related_name='content_comments')
-
+    user_comments = models.ManyToManyField('UserComment', related_name='content_comments', blank=True, default=None)
+    
+    class Meta:
+        verbose_name = 'Content'
+        verbose_name_plural = 'Contents'
+        ordering = ('id',)
+    
     def __str__(self):
         return self.title
 
@@ -29,11 +30,16 @@ class Video(models.Model):
     video = models.FileField(upload_to='videos/')
 
 class UserComment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_user_comments')
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     comment_text = models.TextField()
     enabled = models.BooleanField(default=False)
     approved_by_admin = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = 'UserComment'
+        verbose_name_plural = 'UserComments'
+        ordering = ('id',)
 
     def __str__(self):
         return f"{self.user.username} - {self.comment_text}"
