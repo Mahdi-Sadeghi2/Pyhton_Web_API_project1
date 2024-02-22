4947b99a07c358fd33e5ccd0023208e150da4f2d
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-22 14:42:05 +0330

    Added view to financial app

--- financial/views.py
+++ financial/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from .models import Payment
+from cart.models import ShoppingCart
 
-# Create your views here.
+
+class PaymentListView(View):
+    def get(self, request):
+        payments = Payment.objects.all()
+        return render(request, 'financial/payment_list.html', {'payments': payments})
+
+class PaymentDetailView(View):
+    def get(self, request, pk):
+        payment = get_object_or_404(Payment, pk=pk)
+        return render(request, 'financial/payment_detail.html', {'payment': payment})
+
+def simulate_successful_payment(request, payment_id):
+    payment = get_object_or_404(Payment, pk=payment_id)
+    payment.payment_status = 'success'
+    payment.save()
+    return redirect('payment_result', payment_id=payment.id)
+
+def simulate_unsuccessful_payment(request, payment_id):
+    payment = get_object_or_404(Payment, pk=payment_id)
+    payment.payment_status = 'failure'
+    payment.save()
+    return redirect('payment_result', payment_id=payment.id)
+
+def payment_result(request, payment_id):
+    payment = get_object_or_404(Payment, pk=payment_id)
+    return render(request, 'financial/payment_result.html', {'payment': payment})
