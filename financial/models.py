05fa1943972bdf5bf09f42e873543783d1e03d67
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-02-22 14:41:32 +0330

    Added models to financial app

--- financial/models.py
+++ financial/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
-# Create your models here.
-# financial/models.py
-from django.db import models
 from cart.models import ShoppingCart
 from django.contrib.auth.models import User
+
+# Create your models here.
+
 
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,6 +11,11 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_status = models.CharField(max_length=20, choices=[('success', 'Success'), ('failure', 'Failure')])
     payment_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+        ordering = ('id',)
 
     def __str__(self):
         return f"{self.user.username} - {self.amount} - {self.payment_status}"
